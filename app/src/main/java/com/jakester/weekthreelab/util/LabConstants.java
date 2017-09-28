b353efd6cbaf11e1aac0a7a94551869d15b2d63c
Jake Rushing <jakester0812@gmail.com>
2017-09-27 20:36:30 -0700

    Initial commit

---
+++ app/src/main/java/com/jakester/weekthreelab/util/LabConstants.java
@@ -0,0 +1,10 @@
+package com.jakester.weekthreelab.util;
+
+/**
+ * Created by Jake on 9/27/2017.
+ */
+
+public class LabConstants {
+    public static final String APPLICATION_ID = "simplechat-client";
+    public static final String SERVER_STRING = "https://codepath-chat-lab.herokuapp.com/parse/";
+}
